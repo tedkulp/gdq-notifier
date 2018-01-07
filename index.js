5be5e72adc25175b6f4674200144989a6dd050a9
Ted Kulp <ted@tedkulp.com>
2018-01-07 09:17:53 -0500

    Multiple changes
    * Added redis for sent notifications
    * Turned off debug messages
    * Set time back to 10 minutes
    * Removed static html call

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 const CronJob = require('cron').CronJob;
 const fs = require('fs');
 
+const _ = require('lodash');
+const Promise = require('bluebird');
+const request = require('request');
+const moment = require('moment');
+const redis = require("redis");
+const client = redis.createClient({
+    host: process.env.DOCKER_REDIS_HOST,
+    port: process.env.DOCKER_REDIS_PORT,
+});
+
 const scraper = require('./src/scraper');
 const notifier = require('./src/notifier');
 
-// TODO: Remove me
-const htmlData = fs.readFileSync('./test/data/schedule.html');
+let isList = false;
+for (let j = 0; j < process.argv.length; j++) {  
+    if (process.argv[j] === "--list") {
+        isList = true;
+    }
+}
 
 let parsedData = [];
 
-const scraperJob = new CronJob({
-    cronTime: '*/5 * * * * *',
-    onTick: () => {
-        parsedData = scraper.parseSchedule(htmlData);
-    },
-    start: true,
-});
+const callScraper = () => {
+    return new Promise((resolve, reject) => {
+        request('https://gamesdonequick.com/schedule', (error, response, body) => {
+            parsedData = scraper.parseSchedule(body);
+            resolve(parsedData);
+        });
+    });
+};
 
-const notifierJob = new CronJob({
-    cronTime: '*/15 * * * * *',
-    onTick: () => {
-        notifier.checkForNotifications(parsedData);
-    },
-    start: true,
-});
+if (isList) {
+    callScraper().then(data => {
+        _.forEach(data, game => {
+            console.log(game.id, game.title, '-', moment(game.startTime).format('MMM Do, h:mm a'));
+        });
+        process.exit();
+    });
+} else {
+    const scraperJob = new CronJob({
+        cronTime: '0 */10 * * * *',
+        onTick: () => {
+            callScraper();
+        },
+        start: true,
+    });
+    
+    const notifierJob = new CronJob({
+        cronTime: '0 * * * * *',
+        onTick: () => {
+            // TODO: 1 is the user id.  Change me for multiple users.
+            notifier.checkForNotifications(parsedData, client, '1');
+        },
+        start: true,
+    });
+
+    callScraper();
+}
--- src/notifier.js
+++ src/notifier.js
@@ -1,26 +1,42 @@
 const _ = require('lodash');
 const moment = require('moment');
+const Promise = require('bluebird');
 const PushBullet = require('pushbullet');
 const pusher = new PushBullet(process.env.PUSHBULLET_API_KEY);
 
 const gamesICareAbout = [
     '4a9e2013c349e74f7528455a189eb5433b9967b9',
+    '2caeb6a99e5f965c3ebd31c510a076d854fddead',
     '3068e7bc7f11224098b676c294e65505e5e63f4e',
+    '47f2ca191cd37fa6bb62e2040421305c0e181870',
+    '152c4cd781ffbff99d17b58d117c434ac6c7a587',
+    '8f2bea95c70ed278484fcb666d3377e1f2871b5d',
 ];
 
-const previousNotifications = [];
+const setNotificationAsSent = (redis, userId, gameId) => {
+    const keyName = `sentnotifications:${userId}`;
+    redis.rpush(keyName, gameId);
+};
 
-const checkForNotifications = parsedData => {
-    //const checkTime = moment.add(10, 'minutes');
-    const checkTime = moment().add(24, 'hours');
+const checkIfNotificationSent = (redis, userId, gameId) => {
+    return new Promise((resolve, reject) => {
+        const keyName = `sentnotifications:${userId}`;
+        const existingGames = redis.lrange(keyName, 0, -1, (err, res) => {
+            resolve(_.includes(res, gameId));
+        });
+    });
+}
+
+const checkForNotifications = (parsedData, redis, userId) => {
+    const checkTime = moment().add(10, 'minutes');
     const foundGames = _.filter(parsedData, game => _.includes(gamesICareAbout, game.id));
     _.forEach(foundGames, game => {
-        if (!_.includes(previousNotifications, game.id)) {
-            if (moment(game.startTime).isSameOrBefore(checkTime)) {
-                pusher.note({}, `${game.event} Notification`, `${game.title} by ${game.runners} is going to start in a few minutes.`);
-                previousNotifications.push(game.id);
+        checkIfNotificationSent(redis, userId, game.id).then(wasNotified => {
+            if (!wasNotified && moment(game.startTime).isSameOrBefore(checkTime)) {
+                pusher.note({}, `${game.event} Notification`, `${game.title} by ${game.runners} is going to start at ${moment(game.startTime).format('h:mm a')}.`);
+                setNotificationAsSent(redis, userId, game.id);
             }
-        }
+        });
     });
 };
 
