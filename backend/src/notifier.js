5757fb6f0bbb3c86d919bdc8aba3827bd13472d9
Ted Kulp <ted@tedkulp.com>
2019-06-23 10:27:05 -0400

    Optional email (for sending to SMS service)

--- backend/src/notifier.js
+++ backend/src/notifier.js
@@ -1,12 +1,17 @@
 const _ = require('lodash');
-const fs = require('fs');
-const os = require('os');
 const moment = require('moment-timezone');
 const Promise = require('bluebird');
 const PushBullet = require('pushbullet');
+const nodemailer = require('nodemailer');
 const pusher = new PushBullet(process.env.PUSHBULLET_API_KEY);
 
 const api = require('./api');
+
+let useEmail = false;
+
+if (process.env.EMAIL_ADDRESS && process.env.EMAIL_SERVER && process.env.EMAIL_USERNAME && process.env.EMAIL_PASSWORD) {
+    useEmail = true;
+}
 
 const setNotificationAsSent = (redis, userId, gameId) => {
     const keyName = `sentnotifications:${userId}`;
@@ -22,6 +27,36 @@ const checkIfNotificationSent = (redis, userId, gameId) => {
     });
 };
 
+const createMessage = game => {
+    return `${game.event} - ${game.title}`, `${game.title} by ${game.runners} is going to start at ${moment(game.startTime).tz(process.env.TIMEZONE || 'America/New_York').format('h:mm a')}.`;
+};
+
+const sendPushbulletMessage = game => {
+    pusher.note({}, createMessage(game));
+};
+
+const sendEmail = game => {
+    if (useEmail) {
+        const port = process.env.EMAIL_PORT || 25;
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_SERVER,
+            port: port,
+            secure: port === 465, // true for 465, false for other ports
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+
+        transporter.sendMail({
+            from: '"GDQ Notifier" <no-reply@tedkulp.com>', // sender address
+            to: process.env.EMAIL_ADDRESS, // list of receivers
+            subject: "GDQ Update", // Subject line
+            text: createMessage(game), // plain text body
+        })
+    }
+};
+
 const checkForNotifications = (parsedData, redis, userId) => {
     const checkTime = moment().add(10, 'minutes');
 
@@ -30,7 +65,8 @@ const checkForNotifications = (parsedData, redis, userId) => {
         _.forEach(foundGames, game => {
             checkIfNotificationSent(redis, userId, game.id).then(wasNotified => {
                 if (!wasNotified && moment(game.startTime).isSameOrBefore(checkTime)) {
-                    pusher.note({}, `${game.event} - ${game.title}`, `${game.title} by ${game.runners} is going to start at ${moment(game.startTime).tz(process.env.TIMEZONE || 'America/New_York').format('h:mm a')}.`);
+                    sendPushbulletMessage(game);
+                    sendEmail(game);
                     setNotificationAsSent(redis, userId, game.id);
                 }
             });
